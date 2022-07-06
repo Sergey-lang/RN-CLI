8e7312202bcdb455fc4985ab46b1e76d09a82d00
Sergey <sergei.kuharyonok@yandex.by>
2022-07-06 23:57:18 +0300

    Navigations

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Pressable, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function AScreen({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('B');
+  };
   return (
-    <View>
-      <Text>React CLI</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>A Screen</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => ({
+          backgroundColor: pressed ? '#ffffff' : 'green',
+        })}>
+        <Text>Go to B Screen</Text>
+      </Pressable>
     </View>
+  );
+}
+
+function BScreen({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('A');
+  };
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>B Screen</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => ({
+          backgroundColor: pressed ? '#ffffff' : 'green',
+        })}>
+        <Text>Go to A Screen</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="A" component={AScreen} />
+        <Stack.Screen name="B" component={BScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
